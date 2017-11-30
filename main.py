8e76b18702fdcdd1bc3ed3dd239626ea0849b00f
Shun Arahata <arahatashun@users.noreply.github.com>
2017-11-30 20:19:13 +0900

    debug

--- main.py
+++ main.py
@@ -20,16 +20,27 @@ def getStiffnerCounts(rib_distance,stiffner_distance):
     :param rib_distance:リブの間隙
     :param stiffner_distance:スティフナーの距離
     """
+    stiffner_counts=rib_distance//stiffner_distance
+    return stiffner_counts
 
 def calcsta625():
+    sta625=625
     y=sm_df.ix[0][1]
     Sf=sm_df.ix[0][4]
-    print("Sf",Sf)
+    #print("Sf",Sf)
     Mf=sm_df.ix[0][5]
-    print("Mf",Mf)
-    print(getHf(685))
-
-
+    #print("Mf",Mf)
+    #print(getHf(685))
+    """
+    以下パラメーターの設定
+    """
+    stiffner_thickness=2.03#mm
+    stiffner_bs1=65
+    stiffner_bs2=20
+    web_thickness=18
+    web_distance=60
+    hf=getHf(sta625+web_distance)
+    print(hf)
 
 
 
