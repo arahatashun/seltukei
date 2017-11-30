e6015be2ddcb6274e62876406c1facd480e7b54a
Shun Arahata <arahatashun@users.noreply.github.com>
2017-11-30 14:35:29 +0900

    debug

--- web.py
+++ web.py
@@ -42,6 +42,16 @@ class Web(object):
         return self.getBucklingShearForce()/self.getShearForce(qmax)-1
 
 
+    def getWebHoleLossMS(self,p,D):
+        """
+        ウェブホールロスの計算
+        :param p:リベット間隔
+        :param D:リベットの直径[mm]
+        """
+        f_sj=self.getShearForce()*p/(p-d)
+        ms=self.getBucklingShearForce()/f_sj-1
+
+
 
 """
 def test():
