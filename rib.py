b94761543f3313abf90b3e22441252b9efd20005
Shun Arahata <arahatashun@users.noreply.github.com>
2017-12-01 13:19:55 +0900

    omi

--- rib.py
+++ rib.py
@@ -13,6 +13,18 @@ from rivet_web_stiffner import RivetWebStiffner
 
 
 class Rib(object):
+    """
+    Ribのクラス
+    分割数からのスティフナー間隔の計算やheの計算を主に受け持つ
+    メンバ関数としては
+    Componentオブジェクトの生成と
+    csv のwriterオブジェクトを受け取るを追加する
+    """
+    def __init__(y):
+        """
+        :param y:staの値を受け取る
+        """
+        self.y_=y
 
 
 
