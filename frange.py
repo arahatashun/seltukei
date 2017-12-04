90b9e496582460a6011faa1558977b70c6aacc17
Shun Arahata <arahatashun@users.noreply.github.com>
2017-12-04 21:34:38 +0900

    unti

--- frange.py
+++ frange.py
@@ -1,14 +1,15 @@
+"""Frange Base Class."""
 # coding:utf-8
 # Author: Shun Arahata
-import scipy as sp
-import numpy as np
-import math
-from unit_convert import *
+from unit_convert import inch2mm
 
 
 class Frange:
+    """Frange Base Class."""
+
     def __init__(self, thickness, b_bottome, b_height):
-        """
+        """Constructor.
+
         :param thickness:フランジ厚さ
         :param b_bottom:フランジ底長さ
         :param b_height:フランジ高さ
@@ -17,11 +18,16 @@ class Frange:
         self.b_bottom_ = b_bottome
         self.b_height_ = b_height
 
-    def getArea(self, web_thickness):
+    def get_area(self, web_thickness):
+        """Get Area of Frange.
+
+        :@param web_thickness:web厚さ
+        """
         return (self.b_bottom_ + self.b_height_) * self.thickness_ + web_thickness**2 * 30
 
-    # 取り付け場所からのheを求めるための排除長さ
-    def getCenterOfGravity(self):
+
+    def get_center_of_gravity(self):
+        """フランジの底から重心高さを求める.heを求める."""
         area = (self.b_bottom_ + self.b_height_) * self.thickness_
         bottom_area = (self.b_bottom_ + self.thickness_ / 2) * self.thickness_
         residure = area / 2 - bottom_area
@@ -30,10 +36,11 @@ class Frange:
             # print("residure<0")
             return area / 2 / (self.b_bottom_ + self.thickness_ / 2)
         else:
-            return self.b_bottom_ + residure / self.thickness_
+            return self.thickness_ + residure / self.thickness_
 
-    def getAxialForce(self, momentum, h_e):
-        """
+    def get_axial_force(self, momentum, h_e):
+        """ フランジの軸力を求める.
+
         :param momentum:前桁負担分モーメント
         :param h_e:桁フランジ断面重心距離
         :return axialforce: [N]
@@ -41,28 +48,29 @@ class Frange:
         axialforce = momentum / h_e * 1000
         return axialforce
 
-    # f_c,f_t
-    def getStressForce(self, momentum, h_e, web_thickness):
-        """
+    def get_stress_force(self, momentum, h_e, web_thickness):
+        """Get f_c or f_t.
+
         :param momentum:前桁分担曲げモーメント
         :param h_e:桁フランジ断面重心距離
         :param web_thickness:ウェブ厚さ
         """
-        #[MPa]
-        return self.getAxialForce(momentum, h_e) / self.getArea(web_thickness)
+        #  [MPa]
+        return self.get_axial_force(momentum, h_e) / self.get_area(web_thickness)
 
 
-def test_frange():
+def main():
+    """Test Function."""
     test = Frange(6.0, 34.5, 34.5)
-    A = test.getArea(2.03)
+    A = test.get_area(2.03)
     print("A", A)
-    cofg = test.getCenterOfGravity()
+    cofg = test.get_center_of_gravity()
     print("cofg", cofg)
-    ax = test.getAxialForce(74623, 297)
+    ax = test.get_axial_force(74623, 297)
     print("P[N]", ax)
-    f = test.getStressForce(74623, 297, 2.03)
+    f = test.get_stress_force(74623, 297, 2.03)
     print("fc[MPa]", f)
 
 
 if __name__ == '__main__':
-    test_frange()
+    main()
